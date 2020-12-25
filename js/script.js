1bf5e1470d8ea71961eb50c4732d7d208b4162b2
sabbirhossain-dev <hossainsabbir309@gmail.com>
2020-12-25 20:23:20 +0600

    responsive done

--- js/script.js
+++ js/script.js
@@ -23,25 +23,25 @@ $('.client-slider').slick({
     dots: true,
     infinite: true,
     autoplay:true,
-    speed: 600,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 992,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
     },
         {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
     },
         {
